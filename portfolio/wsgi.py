3e9951248d3c1f06e3b641783e395dda9a2911d1
adbrum <adbrum@outlook.com>
2016-02-05 16:50:45 +0000

    Procfile

--- portfolio/wsgi.py
+++ portfolio/wsgi.py
@@ -9,9 +9,8 @@ https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
 
 import os
 from django.core.wsgi import get_wsgi_application
+from dj_static import Cling
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "portfolio.settings")
-
-from dj_static import Cling
 
 application = Cling(get_wsgi_application())
